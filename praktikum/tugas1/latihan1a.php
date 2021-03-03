ce816a9da30f1778f04c435804d6d893c68ae278
Rizky Mahendra <rizkimahendra117@gmail.com>
2021-03-03 16:54:29 +0700

    modul 1 praktikum

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
+
+?>
+
+
+<!-- Nama : Rizky Mahendra
+NRP : 203040094
+Shift Praktikum : Kamis pukul 08:00 -->
